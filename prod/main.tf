4662515cecde98010f98b5f9cf2859c4dde4c340
huugosousa <145609526+huugosousa@users.noreply.github.com>
2023-09-20 20:52:08 -0300

    Add files via upload

---
+++ prod/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    
+    default_tags {
+        tags = {
+            Owner = "Hugo Sousa"
+            Env   = "Producao New Teste"
+            Class = "CLC10"
+        }
+    }
+  # Configuration options
+}
